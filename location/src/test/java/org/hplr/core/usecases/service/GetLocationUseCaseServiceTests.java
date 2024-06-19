3e14b19a595ded39f6451166d5855e68d279ea33
Sedarius-1 <jakubkalinowski1234@gmail.com>
2024-06-19 19:59:27 +0200

    github page fix v5

---
+++ location/src/test/java/org/hplr/core/usecases/service/GetLocationUseCaseServiceTests.java
@@ -0,0 +1,12 @@
+package org.hplr.core.usecases.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class GetLocationUseCaseServiceTests {
+    //todo: make this
+    @Test
+    void assert_truth(){
+        Assertions.assertTrue(true);
+    }
+}
